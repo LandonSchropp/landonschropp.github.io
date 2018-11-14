b8b72d95e0d969db77ea3a3cb877fd3b85efca9b
Landon Schropp <schroppl@gmail.com>
2018-11-14 01:18:38 -0800

    Remove blacklisted plugin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,7 +2,8 @@ import connect from 'gulp-connect';
 import data from 'gulp-data';
 import frontMatter from 'gulp-front-matter';
 import gulp from 'gulp';
-import insert from 'gulp-insert';
+import header from 'gulp-header';
+import footer from 'gulp-footer';
 import markdown from 'gulp-markdown';
 import merge from 'merge2';
 import nunjucksRender from 'gulp-nunjucks-render';
@@ -10,14 +11,13 @@ import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import sitemap from 'gulp-sitemap';
 
+// Rather than creating separate tasks and duplicating steps, or creating temporary files, this task
+// separately creates the notes and regular HTML streams. It then merged the streams together for
+// the final processing. This seems to be the recommended appraoch in the Gulp documentation.
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/using-multiple-sources-in-one-task.md
 gulp.task('html', () => {
 
-  // * Parse the front matter
-  // * Strip the date from the file name and add it to the front matter
-  // * Compile the notes to nunjucks
-  // * Wrap the nunjucks content in a content block and set the property variable
-  // * Combine the nunjucks junk with the HTML stream using merge-stream
-  // TODO: Add a warning if the slug is already taken
+  // TODO: Throw an error if the slug is already taken
   let notes = gulp.src('source/html/notes/*', { base: 'source/html/' })
     .pipe(frontMatter({ property: 'data', remove: true }))
     .pipe(data(file => ({
@@ -26,14 +26,10 @@ gulp.task('html', () => {
     })))
     .pipe(rename(path => path.basename = path.basename.replace(/(\d{4}-\d{2}-\d{2})-/, '')))
     .pipe(markdown())
-    .pipe(insert.wrap("{% extends 'layouts/note.njk' %}{% block article %}", "{% endblock %}"));
+    .pipe(header("{% extends 'layouts/note.njk' %}{% block article %}"))
+    .pipe(footer("{% endblock %}"));
 
   let html = gulp.src('source/html/pages/**');
-
-  // Rather than creating separate tasks and duplicating steps, or creating temporary files, I've
-  // decided to handle notes and HTML files separate and then merge them together for the common
-  // logic. This seems to be the recommended appraoch in the Gulp documentation.
-  // https://github.com/gulpjs/gulp/blob/master/docs/recipes/using-multiple-sources-in-one-task.md
   return merge(notes, html)
     .pipe(plumber())
     .pipe(nunjucksRender({ path: [ "source/html" ] }))
