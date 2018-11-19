9506c8f267e658dd5fe69078854f6bf2fe56b472
Landon Schropp <schroppl@gmail.com>
2018-11-18 17:28:02 -0800

    Add glob imports to index.sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 const autoprefixer = require('autoprefixer');
-const postcssImport = require('postcss-import');
+const postcssEasyImport = require('postcss-easy-import');
 
 module.exports = {
   plugins: [
     autoprefixer('last 2 versions'),
-    postcssImport
+    postcssEasyImport
   ]
 };
