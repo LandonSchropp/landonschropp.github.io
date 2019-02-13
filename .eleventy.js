2a7a75120c15764b46d3176c238b3cd7a4b238ee
Landon Schropp <schroppl@gmail.com>
2019-02-13 00:13:57 -0800

    Clean up the layouts
    * Remove the layout aliases
    * Rename the default layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
 // TODO: Figure out how to move the root pages into a separate `pages` directory.
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addLayoutAlias("default", "layouts/layout.njk");
-  eleventyConfig.addLayoutAlias("note", "layouts/note.njk");
-
   return {
     dir: {
       input: "source",
