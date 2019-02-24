7f6e72e3d7b43edece19e1989ee7ba5e73c9fe2b
Landon Schropp <schroppl@gmail.com>
2019-02-24 13:46:50 -0800

    Add Embedly tag

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const embedly = require('./lib/embedly');
+
 // TODO: Figure out how to move the root pages into a separate `pages` directory.
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addLayoutAlias("default", "layouts/layout.njk");
+  eleventyConfig.addLayoutAlias("note", "layouts/note.njk");
+
+  eleventyConfig.addNunjucksTag("embedly", embedly);
 
   return {
     dir: {
