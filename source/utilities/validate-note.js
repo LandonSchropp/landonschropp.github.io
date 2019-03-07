6d3fd9fdd008928b37d65c01973cdf755c073b24
Landon Schropp <schroppl@gmail.com>
2019-03-06 19:38:55 -0800

    Consolidate the utilities

--- source/utilities/validate-note.js
+++ source/utilities/validate-note.js
@@ -1,9 +1,36 @@
-const noteDataSchema = require('./note-data-schema');
 const Joi = require('joi');
+
+let schema = Joi
+  .object({
+    type: Joi.string().valid("article", "video", "podcast", "talk").required(),
+
+    title: Joi.string().required(),
+
+    // The author can be a string or non-empty array of strings
+    author: Joi.alternatives([
+      Joi.string(),
+      Joi.array().items(Joi.string()).min(1)
+    ]).required(),
+
+    sourceName: Joi.string().required(),
+
+    // The source URL is requried unless it's a live talk
+    sourceURL: Joi.when("live", {
+      is: Joi.not(true),
+      then: Joi.string().uri().required()
+    }),
+
+    // Live is required for all talks
+    live: Joi.when("type", {
+      is: "talk",
+      then: Joi.boolean().required()
+    })
+  })
+  .unknown();
 
 module.exports = function validateNote({ inputPath, data }) {
 
-  let { error } = Joi.validate(data, noteDataSchema);
+  let { error } = Joi.validate(data, schema);
 
   if (error) {
     throw new Error(
