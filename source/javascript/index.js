c6865c1b47a515b27b7254ec96b1ca96d599e362
Landon Schropp <schroppl@gmail.com>
2019-03-09 23:55:34 -0800

    Reorganize page JavaScript

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -1,35 +1,11 @@
-import { updateViewBox, updateElements } from './dom';
-
-function currentTemplate() {
-  return window.matchMedia("(max-aspect-ratio: 1 / 1)").matches
-    ? "portrait"
-    : "landscape";
-}
-
-let lastTemplate = null;
-
-function update() {
-  let template = currentTemplate();
-
-  if (template === lastTemplate) {
-    return;
-  }
-
-  let landing = document.querySelector(".landing");
-  let paths = landing.querySelectorAll("path[id], polygon[id]");
-
-  lastTemplate = template;
-  updateViewBox(template, landing);
-  updateElements(template, paths);
-}
+import landingPage from './landing-page';
+import notesPage from './notes-page';
 
 // If we're not on the landing page, don't do anything
 if (window.location.pathname === '/') {
+  landingPage();
+}
 
-  // Update the landing SVG right away. We don't need a ready even because this script is loaded
-  // after the landing SVG.
-  update();
-
-  // Whenever the window size changes, update the viewBox.
-  window.addEventListener('resize', update);
+if (window.location.pathname === "/notes/") {
+  notesPage();
 }
