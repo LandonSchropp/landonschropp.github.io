f4776b1310ea3ecd53af217d5c3861b0f2737aae
Landon Schropp <schroppl@gmail.com>
2019-03-18 21:31:36 -0700

    Replce the baseURL implementation

--- source/eleventy/base-url.js
+++ source/eleventy/base-url.js
@@ -1,6 +1,4 @@
 // This function takes a URL and returns the base URL.
-// TODO: Do this the right way with JavaScript.
 module.exports = function baseURL(url) {
-  let match = url.match(/.+:\/\/[^/]+/);
-  return match ? match[0] : null;
+  return new URL(url).origin;
 };
