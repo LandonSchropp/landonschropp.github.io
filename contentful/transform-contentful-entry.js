62c872af6373b9944ec8383e5545db33d750e24f
Landon Schropp <schroppl@gmail.com>
2019-04-02 21:34:08 -0700

    Generate descriptions for the notes

--- contentful/transform-contentful-entry.js
+++ contentful/transform-contentful-entry.js
@@ -1,11 +1,18 @@
 import marked from './marked';
+import listify from '../eleventy/filters/listify';
 
 export default function transformContentfulEntry({ fields }) {
-  let { content, date, ...data } = fields;
+  let { content, date, title, authors, ...data } = fields;
+
+  // Generate a description automatically using the title.
+  let description = `These are my personal notes from ${ title } by ${ listify(authors) }.`;
 
   return {
     ...data,
+    title,
+    authors,
     date: new Date(date),
-    content: marked(content)
+    content: marked(content),
+    description
   };
 }
