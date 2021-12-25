6643841993dfab05be60f65c46c076b6fc403310
Landon Schropp <schroppl@gmail.com>
2021-12-24 22:21:54 -0600

    Add Google Fonts

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -8,6 +8,18 @@ export function Layout({ children, navigation }) {
       <title>Landon Schropp</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" type="image/png" href="/images/flannel.png" />
+
+      {/* Google Fonts */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href={
+          "https://fonts.googleapis.com/css2?family=Gentium+Book+Basic:ital,wght@0,400;0,700;1,"
+          + "400;1,700&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family="
+          + "Source+Code+Pro:ital@0;1&display=swap"
+        }
+        rel="stylesheet"
+      />
     </Helmet>
     {children}
   </>;
