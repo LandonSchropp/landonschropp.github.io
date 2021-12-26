0e04c6d346d7c59597b7b8bb17eb423e3651c827
Landon Schropp <schroppl@gmail.com>
2021-12-26 17:12:04 -0600

    Add media queries

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@react-hook/media-query";
 import React from "react";
 
 import flannel from "../images/flannel.png";
@@ -5,8 +6,13 @@ import Landscape from "../images/landing/landscape.svg";
 import Portrait from "../images/landing/portrait.svg";
 import { Layout } from "../layout/layout";
 
+const PORTRAIT_MEDIA_QUERY = "(max-aspect-ratio: 1 / 1)";
+
 export default function IndexPage() {
+  const isPortrait = useMediaQuery(PORTRAIT_MEDIA_QUERY);
+
   return <Layout navigation={ false }>
+    { isPortrait ? <Portrait /> : <Landscape /> }
     <Landscape />
   </Layout>;
 }
