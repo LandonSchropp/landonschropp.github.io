741d81efe2d6384fd47f66e25b152b9669d13dee
Landon Schropp <schroppl@gmail.com>
2022-01-03 16:23:43 -0600

    Add note data

---
+++ src/data/notes.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+
+/**
+ * Transforms a GraphQL node into a not object that can be used in a note component.
+ */
+export function transformNote({ node }) {
+  return {
+    title: node.title,
+    slug: node.properties["Slug"].value,
+    authors: node.properties["Authors"].value.split(",").map(_.trim),
+    date: new Date(node.properties["Date"].value.start),
+    category: node.properties["Category"].value.name,
+    source: node.properties["Source"].value,
+    url: node.properties["URL"].value
+  };
+}
--- src/pages/notes.jsx
+++ src/pages/notes.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import { useNotes } from "../hooks/use-notes";
 import { Layout } from "../layout/layout";
 
 export default function NotesPage() {
+  let notes = useNotes();
+
   return <Layout navigation={ false } className="notes-page">
     <h1>Notes</h1>
+
+    {
+      notes.map(({ title, slug }) => <h2 key={ slug }>{ title }</h2>)
+    }
   </Layout>;
 }
