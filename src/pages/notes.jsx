593ee527f5843d46fa1b0719f698fe379c8af5eb
Landon Schropp <schroppl@gmail.com>
2022-01-03 14:30:04 -0600

    Add notes skeleton

---
+++ src/pages/notes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Layout } from "../layout/layout";
+
+export default function NotesPage() {
+  return <Layout navigation={ false } className="notes-page">
+    <h1>Notes</h1>
+  </Layout>;
+}
