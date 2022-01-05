cca0c94ab37a0502b1c1c4bbbda32371f761cef5
Landon Schropp <schroppl@gmail.com>
2022-01-05 15:21:56 -0600

    Remove files unrelated to issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,3 @@
-const BLOG_NOTION_ID = "c68575f91f534048bb15c54f0f230882";
-const NOTES_NOTION_ID = "da4f9ded813b424e83e5f552b1f41a3e";
-
 module.exports = {
   siteMetadata: {
     siteUrl: "https://landonschropp.github.io",
@@ -50,25 +47,6 @@ module.exports = {
           include: /icons/
         }
       }
-    },
-    {
-      resolve: "gatsby-source-notion-api",
-      options: {
-        token: process.env.NOTION_API_TOKEN,
-        databaseId: NOTES_NOTION_ID,
-        propsToFrontmatter: true,
-        lowerTitleLevel: true
-      }
-    },
-    {
-      resolve: "gatsby-source-notion-api",
-      options: {
-        token: process.env.NOTION_API_TOKEN,
-        databaseId: BLOG_NOTION_ID,
-        propsToFrontmatter: true,
-        lowerTitleLevel: true
-      }
-    },
-    "gatsby-transformer-note"
+    }
   ]
 };
