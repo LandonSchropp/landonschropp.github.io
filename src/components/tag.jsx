4d9762366fab105c57a8b10577a0ae3200a00a71
Landon Schropp <schroppl@gmail.com>
2022-01-06 15:20:33 -0600

    Filter categories on the notes page

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -1,13 +1,20 @@
-import { Link } from "gatsby";
+import classNames from "classnames";
 import PropTypes from "prop-types";
 import React from "react";
 
-export function Tag({ category }) {
-  return <Link className="tag" to={ `/notes/?category=${ category }` } data-category={ category }>
+export function Tag({ category, onClick, selected }) {
+  return <button
+    type="button"
+    className={ classNames("tag", { "tag--selected": selected }) }
+    data-category={ category }
+    onClick={ onClick }
+  >
     { category }
-  </Link>;
+  </button>;
 }
 
 Tag.propTypes = {
-  category: PropTypes.string.isRequired
+  category: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired
 };
--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,12 +1,15 @@
-import { graphql } from "gatsby";
+import { useLocation } from "@reach/router";
+import { graphql, navigate } from "gatsby";
+import _ from "lodash";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { NoteSummary } from "../../components/note-summary";
 import { Tag } from "../../components/tag";
 import {
   ARTICLE_CATEGORY,
   BOOK_CATEGORY,
+  OTHER_CATEGORY,
   PODCAST_CATEGORY,
   TALK_CATEGORY,
   VIDEO_CATEGORY
@@ -31,7 +34,34 @@ export const query = graphql`
 `;
 
 export default function NotesPage({ data }) {
-  let notes = data.allNote.nodes;
+
+  const { href } = useLocation();
+  let [ category, setCategory ] = useState(null);
+
+  // If the URL contains a category, remove it and store it in the state.
+  useEffect(() => {
+    const url = new URL(href);
+
+    if (url.searchParams.has("category")) {
+      setCategory(url.searchParams.get("category"));
+      url.searchParams.delete("category");
+      navigate(`${ url.pathname }${ url.search }${ url.hash }`, { replace: true });
+    }
+  }, [ href ]);
+
+  // Filter the notes if a category is selected.
+  // NOTE: I'm using includes here to accomodate the `Live Talk` category, which should be included
+  // by `Talk`.
+  let notes = _.isNil(category)
+    ? data.allNote.nodes
+    : data.allNote.nodes.filter(article => article.category.includes(category));
+
+  // TODO: Remove this once the example post is removed.
+  notes = notes.filter(note => note.title !== "Example");
+
+  function toggleCategory(updatedCategory) {
+    setCategory(category === updatedCategory ? null : updatedCategory);
+  }
 
   return <Layout
     title="Landon Schropp - Notes"
@@ -49,13 +79,38 @@ export default function NotesPage({ data }) {
 
       <div className="header__tags">
         <span className="header__tag-group">
-          <Tag category={ ARTICLE_CATEGORY } />
-          <Tag category={ BOOK_CATEGORY } />
-          <Tag category={ VIDEO_CATEGORY } />
+          <Tag
+            category={ ARTICLE_CATEGORY }
+            onClick={ () => toggleCategory(ARTICLE_CATEGORY) }
+            selected={ category === ARTICLE_CATEGORY }
+          />
+          <Tag
+            category={ BOOK_CATEGORY }
+            onClick={ () => toggleCategory(BOOK_CATEGORY) }
+            selected={ category === BOOK_CATEGORY }
+          />
+          <Tag
+            category={ VIDEO_CATEGORY }
+            onClick={ () => toggleCategory(VIDEO_CATEGORY) }
+            selected={ category === VIDEO_CATEGORY }
+          />
         </span>
         <span className="header__tag-group">
-          <Tag category={ TALK_CATEGORY } />
-          <Tag category={ PODCAST_CATEGORY } />
+          <Tag
+            category={ TALK_CATEGORY }
+            onClick={ () => toggleCategory(TALK_CATEGORY) }
+            selected={ category === TALK_CATEGORY }
+          />
+          <Tag
+            category={ PODCAST_CATEGORY }
+            onClick={ () => toggleCategory(PODCAST_CATEGORY) }
+            selected={ category === PODCAST_CATEGORY }
+          />
+          <Tag
+            category={ OTHER_CATEGORY }
+            onClick={ () => toggleCategory(OTHER_CATEGORY) }
+            selected={ category === OTHER_CATEGORY }
+          />
         </span>
       </div>
     </header>
