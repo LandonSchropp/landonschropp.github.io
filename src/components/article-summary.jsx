f3ca5fe2a011475b5fffad9bd7518cffe8d99929
Landon Schropp <schroppl@gmail.com>
2022-01-08 19:08:12 -0600

    Update the article links

--- src/components/article-summary.jsx
+++ src/components/article-summary.jsx
@@ -1,5 +1,6 @@
 import chroma from "chroma-js";
 import { Link } from "gatsby";
+import _ from "lodash";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -8,16 +9,29 @@ import { ArticlePropType } from "../data/prop-types";
 const CORNFLOWER_BLUE = "#627ff6";
 const BITTERSWEET = "#ff6d71";
 
+function ArticleLink({ article, children, ...props }) {
+  if (_.isNil(article.url)) {
+    return <Link to={ `/articles/${ article.slug }` } { ...props }>{ children }</Link>;
+  }
+
+  return <a href={ article.url } { ...props }>{ children }</a>;
+}
+
+ArticleLink.propTypes = {
+  article: ArticlePropType.isRequired,
+  children: PropTypes.node.isRequired
+};
+
 export function ArticleSummary({ article, index, numberOfArticles }) {
 
   // Ideally this wouldn't need to be done in JavaScript. However, there's not currently a good way
   // to do this in pure CSS, and this effect is col enough that the effort is worth it.
   let color = chroma.mix(CORNFLOWER_BLUE, BITTERSWEET, index / (numberOfArticles - 1));
 
-  return <Link
+  return <ArticleLink
+    article={ article }
     style={ { "--color": color } }
     className="article-summary"
-    to={ `/articles/${ article.slug }` }
   >
     <h3 className="article-summary__title">
       { article.title }
@@ -25,7 +39,7 @@ export function ArticleSummary({ article, index, numberOfArticles }) {
     <p className="article-summary__description">
       { article.description }
     </p>
-  </Link>;
+  </ArticleLink>;
 }
 
 ArticleSummary.propTypes = {
