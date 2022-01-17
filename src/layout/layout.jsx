225fa042319471f7f53b3de93dcb8dc46aa7aefd
Landon Schropp <schroppl@gmail.com>
2022-01-17 14:04:01 -0600

    Update Layout to use the theme Layout component

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,57 +1,32 @@
 import "../stylesheets/index.sass";
 
-import { MDXProvider } from "@mdx-js/react";
 import classNames from "classnames";
+import { Layout as ThemeLayout } from "landon-schropp-theme";
 import PropTypes from "prop-types";
 import React from "react";
 import { Helmet } from "react-helmet";
-import useDarkMode from "use-dark-mode";
 
-import { HighlightedPre } from "../components/highlighted-pre";
-import { MEDIA } from "../data/constants";
+import { CATEGORIES } from "../data/constants";
 import flannel from "../images/flannel.png";
 import { MainNavigation } from "./main-navigation";
 
-// Custom components used in MDX rendering.
-const MDX_COMPONENTS = {
-  pre: HighlightedPre
-};
-
-export function Layout({ children, className, title, description, category, navigation }) {
-  let { value: darkMode } = useDarkMode();
-
-  return <>
+export function Layout({ children, className, category, fullScreen, ...props }) {
+  return <ThemeLayout
+    className={ classNames("main", className) }
+    { ...props }
+    fullScreen={ fullScreen }
+    navigation={ fullScreen ? null : <MainNavigation /> }
+  >
     <Helmet>
-      <title>{ title }</title>
-      <meta name="description" content={ description } />
-
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="shortcut icon" type="image/png" href={ flannel } />
-
-      { /* Google Fonts */ }
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link
-        href={
-          "https://fonts.googleapis.com/css2?family=Gentium+Book+Basic:ital,wght@0,400;0,700;1,"
-          + "400;1,700&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family="
-          + "Source+Code+Pro:ital@0;1&display=swap"
-        }
-        rel="stylesheet"
-      />
-      <body data-category={ category } data-theme={ darkMode ? "dark" : "light" } />
+      <body data-category={ category } />
     </Helmet>
-    { navigation ? <MainNavigation /> : null }
-    <main className={ classNames(className, { "main": navigation }) }>
-      <MDXProvider components={ MDX_COMPONENTS }>
-        { children }
-      </MDXProvider>
-    </main>
-  </>;
+    { children }
+  </ThemeLayout>;
 }
 
 Layout.defaultProps = {
-  navigation: true
+  fullScreen: false
 };
 
 Layout.propTypes = {
@@ -59,6 +34,6 @@ Layout.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  category: PropTypes.oneOf(MEDIA),
-  navigation: PropTypes.bool
+  category: PropTypes.oneOf(CATEGORIES),
+  fullScreen: PropTypes.bool
 };
