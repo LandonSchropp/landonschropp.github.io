2cd5bf0d85f9809dcf4453f7824dcc9c4673fda0
Landon Schropp <schroppl@gmail.com>
2023-12-26 10:53:57 -0600

    Configure Jest for TypeScript

---
+++ jest.setup.ts
@@ -0,0 +1,20 @@
+import { configure } from "@testing-library/dom";
+import { it } from "@jest/globals";
+
+// Monkey patch Jest so that tests written without a function implementation are marked as `todo`.
+type FunctionParameter = (() => void | undefined) | (() => Promise<unknown>);
+
+const wrappedIt = (description: string, func?: FunctionParameter) => {
+  if (!func) {
+    return it.todo(description);
+  }
+
+  return it(description, func);
+};
+
+Object.assign(wrappedIt, it);
+
+globalThis.it = wrappedIt as jest.It;
+
+// Configure the testing library.
+configure({ testIdAttribute: "data-test-id" });
