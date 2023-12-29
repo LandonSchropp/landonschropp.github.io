4c0231060b25801f108b0a407e891fb0ae36530e
Landon Schropp <schroppl@gmail.com>
2023-12-29 12:17:50 -0600

    Fix fonts

--- src/layouts/base-layout.tsx
+++ src/layouts/base-layout.tsx
@@ -21,12 +21,13 @@ export function BaseLayout({ title, description, children }: BaseLayoutProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href={
-            "https://fonts.googleapis.com/css1?family=Gentium+Book+Basic:ital,wght@0,400;0,700;1," +
-            "400;1,700&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family=" +
-            "Source+Code+Pro:ital@0;1&display=swap"
+            "https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;" +
+            "1,400&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family=Source+" +
+            "Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           }
           rel="stylesheet"
         />
+
         <link rel="shortcut icon" type="image/png" href={flannel.src} />
       </head>
       <body className="bg-theme-background text-theme-text flex flex-col h-full *:flex-auto">
