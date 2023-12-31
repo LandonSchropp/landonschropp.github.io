0272b54f9c50407522de241ebe29d3949856cc50
Landon Schropp <schroppl@gmail.com>
2023-12-30 23:13:37 -0600

    Import old constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,33 @@
-export const CATEGORIES = [
-  "Business",
-  "Development",
-  "Design",
-  "Psychology",
-  "Other",
-  "Chess",
-] as const;
+export const ARTICLE_MEDIA = "Article";
+export const BOOK_MEDIA = "Book";
+export const LIVE_TALK_MEDIA = "Live Talk";
+export const OTHER_MEDIA = "Other";
+export const PODCAST_MEDIA = "Podcast";
+export const TALK_MEDIA = "Talk";
+export const VIDEO_MEDIA = "Video";
 
 export const MEDIAS = [
-  "Book",
-  "Video",
-  "Talk",
-  "Live Talk",
-  "Podcast",
-  "Article",
-  "Other",
+  ARTICLE_MEDIA,
+  BOOK_MEDIA,
+  LIVE_TALK_MEDIA,
+  OTHER_MEDIA,
+  PODCAST_MEDIA,
+  TALK_MEDIA,
+  VIDEO_MEDIA,
+] as const;
+
+export const BUSINESS_CATEGORY = "Business";
+export const DEVELOPMENT_CATEGORY = "Development";
+export const DESIGN_CATEGORY = "Design";
+export const PSYCHOLOGY_CATEGORY = "Psychology";
+export const CHESS_CATEGORY = "Chess";
+export const OTHER_CATEGORY = "Other";
+
+export const CATEGORIES = [
+  BUSINESS_CATEGORY,
+  DEVELOPMENT_CATEGORY,
+  DESIGN_CATEGORY,
+  PSYCHOLOGY_CATEGORY,
+  CHESS_CATEGORY,
+  OTHER_CATEGORY,
 ] as const;
--- src/layouts/content-layout.tsx
+++ src/layouts/content-layout.tsx
@@ -13,6 +13,14 @@ import "../styles/content/pre.css";
 import type { Category } from "../types";
 import { BaseLayout } from "./base-layout";
 import tailwindConfig from "../../tailwind.config.js";
+import {
+  BUSINESS_CATEGORY,
+  DEVELOPMENT_CATEGORY,
+  DESIGN_CATEGORY,
+  PSYCHOLOGY_CATEGORY,
+  CHESS_CATEGORY,
+  OTHER_CATEGORY,
+} from "../constants";
 
 type ContentLayoutProps = {
   title: string;
@@ -22,12 +30,12 @@ type ContentLayoutProps = {
 };
 
 const COLORS = {
-  Business: tailwindConfig.theme.colors.cornflower,
-  Development: tailwindConfig.theme.colors.purple,
-  Design: tailwindConfig.theme.colors.amethyst,
-  Psychology: tailwindConfig.theme.colors.mulberry,
-  Chess: tailwindConfig.theme.colors.bittersweet,
-  Other: tailwindConfig.theme.colors.bittersweet,
+  [BUSINESS_CATEGORY]: tailwindConfig.theme.colors.cornflower,
+  [DEVELOPMENT_CATEGORY]: tailwindConfig.theme.colors.purple,
+  [DESIGN_CATEGORY]: tailwindConfig.theme.colors.amethyst,
+  [PSYCHOLOGY_CATEGORY]: tailwindConfig.theme.colors.mulberry,
+  [CHESS_CATEGORY]: tailwindConfig.theme.colors.bittersweet,
+  [OTHER_CATEGORY]: tailwindConfig.theme.colors.bittersweet,
 } as const;
 
 export function ContentLayout({ title, description, children, category }: ContentLayoutProps) {
