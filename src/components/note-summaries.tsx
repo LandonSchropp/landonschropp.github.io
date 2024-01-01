8930d6cfe58c8c91a0d674a8ddd9278f7d5894f9
Landon Schropp <schroppl@gmail.com>
2024-01-01 14:02:24 -0800

    Convert notes page to Tailwind and TypeScript

---
+++ src/components/note-summaries.tsx
@@ -0,0 +1,44 @@
+import type { NoteSummary as NoteSummaryType } from "../types";
+
+import { Header } from "../components/header";
+import { isNil } from "remeda";
+import { Tags } from "./tags";
+import { useCategory } from "../hooks/use-category";
+import { NoteSummary } from "./note-summary";
+import { CHESS_CATEGORY, OTHER_CATEGORY } from "../constants";
+
+type NoteSummariesProps = {
+  noteSummaries: NoteSummaryType[];
+};
+
+export function NoteSummaries({ noteSummaries }: NoteSummariesProps) {
+  const [category] = useCategory();
+
+  // Filter the notes if a category is selected.
+  // NOTE: I'm using includes here to accommodate the `Live Talk` category, which should be included
+  // by `Talk`.
+  const filteredNotes = isNil(category)
+    ? noteSummaries
+    : noteSummaries.filter((note) => {
+      return (
+        (category === OTHER_CATEGORY && note.category === CHESS_CATEGORY) ||
+        note.category.includes(category)
+      );
+    });
+
+  return (
+    <>
+      <Header
+        title="Notes"
+        subText="My personal notes on books, articles, talks, podcasts and more."
+      >
+        <Tags />
+      </Header>
+      <section className="my-8">
+        {filteredNotes.map((note) => (
+          <NoteSummary key={note.slug} note={note} />
+        ))}
+      </section>
+    </>
+  );
+}
