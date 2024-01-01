33bf594aecfe7e474785e97ec60b01cc3f190dcd
Landon Schropp <schroppl@gmail.com>
2024-01-01 12:57:39 -0800

    Extract Tags component

---
+++ src/components/tags.tsx
@@ -0,0 +1,68 @@
+// TODO: Add missing chess category.
+
+import { useCallback } from "react";
+import {
+  BUSINESS_CATEGORY,
+  DEVELOPMENT_CATEGORY,
+  DESIGN_CATEGORY,
+  PSYCHOLOGY_CATEGORY,
+  OTHER_CATEGORY,
+} from "../constants";
+import { useCategory } from "../hooks/use-category";
+import type { Category } from "../types";
+import { Tag } from "./tag";
+
+type TagGroupProps = {
+  children: React.ReactNode;
+};
+
+// The tag groups exist to allow the tags to wrap in a grouped fashion. This prevents one "lone tag"
+// from sitting on the next line on standard mobile sizes.
+function TagGroup({ children }: TagGroupProps) {
+  return <span className="flex gap-2 [&>*]:my-1">{children}</span>;
+}
+
+export function Tags() {
+  const [category, setCategory] = useCategory();
+
+  const toggleCategory = useCallback(
+    (updatedCategory: Category | null) => {
+      setCategory(category === updatedCategory ? null : updatedCategory);
+    },
+    [category, setCategory],
+  );
+
+  return (
+    <div className="flex flex-wrap justify-center gap-2">
+      <TagGroup>
+        <Tag
+          category={BUSINESS_CATEGORY}
+          onClick={toggleCategory}
+          selected={category === BUSINESS_CATEGORY}
+        />
+        <Tag
+          category={DEVELOPMENT_CATEGORY}
+          onClick={toggleCategory}
+          selected={category === DEVELOPMENT_CATEGORY}
+        />
+        <Tag
+          category={DESIGN_CATEGORY}
+          onClick={toggleCategory}
+          selected={category === DESIGN_CATEGORY}
+        />
+      </TagGroup>
+      <TagGroup>
+        <Tag
+          category={PSYCHOLOGY_CATEGORY}
+          onClick={toggleCategory}
+          selected={category === PSYCHOLOGY_CATEGORY}
+        />
+        <Tag
+          category={OTHER_CATEGORY}
+          onClick={toggleCategory}
+          selected={category === OTHER_CATEGORY}
+        />
+      </TagGroup>
+    </div>
+  );
+}
