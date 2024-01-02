e70fd7105906879bed3e9457b0093174b4a428a5
Landon Schropp <schroppl@gmail.com>
2024-01-02 09:53:32 -0800

    Switch notes page to Metadata

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,16 +1,13 @@
 import { NoteSummaries } from "@/components/note-summaries";
 import { fetchNoteSummaries } from "@/data/notes";
-import { ContentLayout } from "@/layouts/content-layout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Landon Schropp - Notes",
+  description: "My personal notes on books, articles, talks, podcasts and more.",
+};
 
 export default async function NotesPage() {
   let noteSummaries = await fetchNoteSummaries();
-
-  return (
-    <ContentLayout
-      title="Landon Schropp - Notes"
-      description="My personal notes on books, articles, talks, podcasts and more."
-    >
-      <NoteSummaries noteSummaries={noteSummaries} />
-    </ContentLayout>
-  );
+  return <NoteSummaries noteSummaries={noteSummaries} />;
 }
