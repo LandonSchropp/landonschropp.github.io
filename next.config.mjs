583203d42c731f67d77bdd361a25a57cc98e9a63
Landon Schropp <schroppl@gmail.com>
2024-01-01 23:49:06 -0800

    Convert project to Next.js

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+export default {
+  output: "export",
+  distDir: "dist",
+
+  webpack(config) {
+    if (!(config.module?.rules instanceof Array)) {
+      throw new Error("Invalid Webpack Configuration");
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      resourceQuery: /react/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
