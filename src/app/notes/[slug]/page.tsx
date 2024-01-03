24a21bf2c52c26d7c43d7855f6e589bbd6886d7f
Landon Schropp <schroppl@gmail.com>
2024-01-02 21:50:47 -0800

    Add metadata to note page

--- src/app/notes/[slug]/page.tsx
+++ src/app/notes/[slug]/page.tsx
@@ -1,9 +1,19 @@
 import { Note } from "@/components/note";
 import { fetchNoteSummaries, fetchNote } from "@/data/notes";
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
   let noteSummaries = await fetchNoteSummaries();
   return noteSummaries.map(({ slug }) => ({ slug }));
+}
+
+export async function generateMetadata({ params: { slug } }: NotePageProps): Promise<Metadata> {
+  let note = await fetchNote(slug);
+
+  return {
+    title: `Landon Schropp -  ${note.title}`,
+    description: `Landon Schropp's notes about ${note.title}`,
+  };
 }
 
 type NotePageProps = {
