1c8d218502f7fd45265244fb42b21724718b3ec4
Landon Schropp <schroppl@gmail.com>
2024-01-03 14:30:40 -0800

    Update SvgDataShape to add invisible rect behind path

--- src/components/svg-data-shape.tsx
+++ src/components/svg-data-shape.tsx
@@ -4,16 +4,27 @@ type SvgDataShapeProps = {
   shape: SvgDataShapeType | null;
 };
 
+const SVG_DATA_SHAPE_CLASS_NAME = "fill-[url('#svg-data-background')]";
+
 export function SvgDataShape({ shape }: SvgDataShapeProps) {
   if (!shape) return null;
 
-  let { type, ...props } = shape;
-
-  let className = "fill-[url('#svg-data-background')]";
-  switch (type) {
-    case "path":
-      return <path className={className} {...props} />;
-    case "polygon":
-      return <polygon className={className} {...props} />;
+  if (shape.type === "polygon") {
+    let { type, ...props } = shape;
+    return <polygon className={SVG_DATA_SHAPE_CLASS_NAME} {...props} />;
   }
+
+  let {
+    type,
+    bounds: [x, y, width, height],
+    ...props
+  } = shape;
+
+  return (
+    <>
+      {/* Add an invisible box so hovers work for the full link area. */}
+      <rect x={x} y={y} width={width} height={height} fill="transparent" />
+      <path className={SVG_DATA_SHAPE_CLASS_NAME} {...props} />;
+    </>
+  );
 }
