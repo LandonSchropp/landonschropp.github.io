848eb608197cab25723f9352cc8802352d1d69a8
Landon Schropp <schroppl@gmail.com>
2024-01-03 17:31:16 -0800

    Normalize headers

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -21,6 +21,29 @@ function fixBlockauotes(markdown: string) {
   });
 }
 
+/**
+ * If the markdown string contains h1s, convert them down a level.
+ */
+function normalizeHeaders(markdown: string): string {
+  // Don't transform the markdown unless it contains an h1.
+  if (!/(?<=\n|^)#\s+\S/.test(markdown)) {
+    return markdown;
+  }
+
+  // Drop all of the headers down a level.
+  return markdown.replaceAll(/(?<=\n|^)#+(?=\s\S)/g, (match) => `${match}#`);
+}
+
+/**
+ * Converts markdown to html.
+ */
+function convertMarkdownToHtml(markdown: string) {
+  return markdownIt.render(markdown);
+}
+
+/**
+ * Converts markdown to HTML, applying a variety of fixes along the way.
+ */
 export function markdownToHtml(markdown: string) {
-  return R.pipe(markdown, fixNewlies, fixBlockauotes, (markdown) => markdownIt.render(markdown));
+  return R.pipe(markdown, fixNewlies, fixBlockauotes, normalizeHeaders, convertMarkdownToHtml);
 }
