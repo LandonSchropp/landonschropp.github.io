e06eed973bdf297743d20f4fc1f5656d9ef1da13
Landon Schropp <schroppl@gmail.com>
2024-01-05 14:35:53 -0800

    Update icons

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,13 +1,15 @@
 import BookIcon from "../images/icons/book.svg?react";
+import ChessComIcon from "../images/icons/chess-com.svg?react";
 import EmailIcon from "../images/icons/email.svg?react";
 import GitHubIcon from "../images/icons/github.svg?react";
-import LinkedInIcon from "../images/icons/linkedin.svg?react";
+import LinkedInIcon from "../images/icons/linked-in.svg?react";
 
 const ICONS = {
   book: BookIcon,
   email: EmailIcon,
   github: GitHubIcon,
   linkedin: LinkedInIcon,
+  chessCom: ChessComIcon,
 } as const;
 
 type IconProps = {
