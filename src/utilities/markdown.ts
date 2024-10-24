6c943272ea7bf7613c469331f5f7465bbb9255b8
Landon Schropp <schroppl@gmail.com>
2024-10-23 21:45:06 -0700

    Remove all of the markdown fixes
    I no longer need these now that I'm not pulling the data from Notion and
    transforming it.

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -8,32 +8,6 @@ const markdownIt = createMarkdownIt({
   },
 });
 
-function fixNewlies(markdown: string) {
-  return markdown.replaceAll(/\n[\n]+/g, "\n\n");
-}
-
-/**
- * This fixes a bug in markdownToHtml where blockquotes are not properly formatted.
- */
-function fixBlockauotes(markdown: string) {
-  return markdown.replaceAll(/>[\s\S]+?(?=(\n\w|$))/g, (match) =>
-    match.replaceAll("\n\n\t", "\n>\n> "),
-  );
-}
-
-/**
- * If the markdown string contains h1s, convert them down a level.
- */
-function normalizeHeaders(markdown: string): string {
-  // Don't transform the markdown unless it contains an h1.
-  if (!/(?<=\n|^)#\s+\S/.test(markdown)) {
-    return markdown;
-  }
-
-  // Drop all of the headers down a level.
-  return markdown.replaceAll(/(?<=\n|^)#+(?=\s\S)/g, (match) => `${match}#`);
-}
-
 /**
  * Converts markdown to html.
  */
@@ -45,5 +19,5 @@ function convertMarkdownToHtml(markdown: string) {
  * Converts markdown to HTML, applying a variety of fixes along the way.
  */
 export function markdownToHtml(markdown: string) {
-  return R.pipe(markdown, fixNewlies, fixBlockauotes, normalizeHeaders, convertMarkdownToHtml);
+  return R.pipe(markdown, convertMarkdownToHtml);
 }
