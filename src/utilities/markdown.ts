d37f15ec0f2b578c9da3a0bb07b85e54c6e274c4
Landon Schropp <schroppl@gmail.com>
2024-11-20 22:35:25 -0800

    Simplify the convertMarkdownToHtml function

--- src/utilities/markdown.ts
+++ src/utilities/markdown.ts
@@ -1,6 +1,5 @@
 import highlightJs from "highlight.js/lib/common";
 import createMarkdownIt from "markdown-it";
-import * as R from "remeda";
 
 const markdownIt = createMarkdownIt({
   highlight: (code, language) => {
@@ -11,13 +10,6 @@ const markdownIt = createMarkdownIt({
 /**
  * Converts markdown to html.
  */
-function convertMarkdownToHtml(markdown: string) {
+export function convertMarkdownToHtml(markdown: string) {
   return markdownIt.render(markdown);
-}
-
-/**
- * Converts markdown to HTML, applying a variety of fixes along the way.
- */
-export function markdownToHtml(markdown: string) {
-  return R.pipe(markdown, convertMarkdownToHtml);
 }
