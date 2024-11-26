9c7b4202d124d71c6a621fa8f3be7f8bb1fddd4d
Landon Schropp <schroppl@gmail.com>
2024-11-25 21:59:07 -0800

    Remove Unraveling Flexbox callout

--- src/components/articles/article-summaries.tsx
+++ src/components/articles/article-summaries.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ArticleSummary } from "@/components/articles/article-summary";
-import { Callout } from "@/components/articles/callout";
 import { Header } from "@/components/content/header";
 import type { Article } from "@/types";
 
@@ -13,11 +12,6 @@ export function ArticleSummaries({ articleSummaries }: ArticleSummariesProps) {
   return (
     <>
       <Header title="Writing" subText="My published articles from all over the web." />
-
-      <Callout>
-        In 2015 I wrote the book <a href="https://unravelingflexbox.com">Unraveling Flexbox</a>, the
-        ultimate guide to building modern CSS layouts with flexbox.
-      </Callout>
 
       <section>
         {articleSummaries.map((articleSummary, index) => {
