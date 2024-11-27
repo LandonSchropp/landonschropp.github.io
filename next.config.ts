80ea99f4d0f537db97d7acc28f57e72f4dcd97cb
Landon Schropp <schroppl@gmail.com>
2024-11-27 12:08:48 -0800

    Disable ESLint rules for Next.js Webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
+/* 
+eslint-disable @typescript-eslint/no-unsafe-call -- This can't be re-enabled until I can drop the
+Webpack configuraiton from Next.js.
+*/
+
+/* 
+eslint-disable @typescript-eslint/no-unsafe-member-access -- This can't be re-enabled until I can
+drop the Webpack configuraiton from Next.js.
+*/
+
 export default {
   output: "export",
   distDir: "dist",
-
   webpack(config) {
     if (!(config.module?.rules instanceof Array)) {
       throw new Error("Invalid Webpack Configuration");
