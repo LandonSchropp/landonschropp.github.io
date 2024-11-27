cce7b07790650425b21637ca734accd7780201ad
Landon Schropp <schroppl@gmail.com>
2024-11-27 11:34:49 -0800

    Update ESLint configuration
    This switches to the new ESLint flag config format and updates all of
    the ESLint dependencies

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import { includeIgnoreFile } from "@eslint/compat";
+import javascript from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import jest from "eslint-plugin-jest";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import typescript from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  includeIgnoreFile(gitignorePath),
+  javascript.configs.recommended,
+  {
+    files: ["**/*.test.{js,ts,jsx,tsx}"],
+    ...jest.configs["flat/recommended"],
+  },
+  reactPlugin.configs.flat.recommended,
+  {
+    plugins: {
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      ...reactHooksPlugin.configs.recommended.rules,
+    },
+    ignores: ["*.test.tsx"],
+  },
+  ...typescript.configs.recommendedTypeChecked,
+  {
+    ignores: ["**/*.d.ts"], // Exclude all .d.ts files
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  prettier,
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "import/no-anonymous-default-export": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+    },
+  },
+];
