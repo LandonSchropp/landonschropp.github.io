3ca6480fa59a048b5c05e55e693841a0321503cd
Landon Schropp <schroppl@gmail.com>
2024-11-29 23:26:05 -0800

    Add validation to ensure content does not contain an H1

--- src/schema/content.ts
+++ src/schema/content.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
+const MarkdownSchema = z
+  .string()
+  .refine((value) => !/^# /m.test(value), { message: "Markdown should not contain an H1" });
+
 export const ContentSchema = z.object({
   title: z.string(),
   slug: z.string().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/),
   date: z.string().date(),
   published: z.boolean(),
-  markdown: z.string(),
+  markdown: MarkdownSchema,
 });
