f458b222c466dac940b41705df52b2a7a09cec84
Landon Schropp <schroppl@gmail.com>
2024-12-29 13:39:48 -0800

    Disable unneeded ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -69,6 +69,7 @@ export default [
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
