7a9b79d62753b3a47b587b83df560edc997a5442
Landon Schropp <schroppl@gmail.com>
2025-01-04 11:35:08 -0800

    Add metadata to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,11 @@
-"use client";
+import { NotFoundPage as ClientSideNotFoundPage } from "@/components/dynamic-pages/not-found-page";
+import { Metadata } from "next";
 
-import { DynamicSVG } from "@/components/dynamic-svg";
-import * as DATA from "@/data/svg";
+export const metadata: Metadata = {
+  title: "404",
+  description: "The page you were looking for couldn't be found.",
+};
 
-// import { Metadata } from "next";
-//
-// export const metadata: Metadata = {
-//   title: "404",
-//   description: "The page you were looking for couldn't be found.",
-// };
-
-export default function NotFoundPage() {
-  return (
-    <DynamicSVG>
-      <DynamicSVG.Aspect key="not-found" minSpacing={0} maxSpacing={0}>
-        <DynamicSVG.Group title="404: Not Found" key="heading" role="heading">
-          <DynamicSVG.Row key="heading" align="top" spacing={0}>
-            <DynamicSVG.Shape key="not-found" {...DATA.notFound} />
-          </DynamicSVG.Row>
-        </DynamicSVG.Group>
-      </DynamicSVG.Aspect>
-    </DynamicSVG>
-  );
+export default function Page() {
+  return <ClientSideNotFoundPage />;
 }
---
+++ src/components/dynamic-pages/not-found-page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { DynamicSVG } from "../dynamic-svg";
+import * as DATA from "@/data/svg";
+import React from "react";
+
+export function NotFoundPage() {
+  return (
+    <DynamicSVG>
+      <DynamicSVG.Aspect key="not-found" minSpacing={0} maxSpacing={0}>
+        <DynamicSVG.Group title="404: Not Found" key="heading" role="heading">
+          <DynamicSVG.Row key="heading" align="top" spacing={0}>
+            <DynamicSVG.Shape key="not-found" {...DATA.notFound} />
+          </DynamicSVG.Row>
+        </DynamicSVG.Group>
+      </DynamicSVG.Aspect>
+    </DynamicSVG>
+  );
+}
