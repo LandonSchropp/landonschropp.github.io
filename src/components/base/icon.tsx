7bbf789da59afa459e14750250e94e617e7f2967
Landon Schropp <schroppl@gmail.com>
2025-01-04 13:57:49 -0800

    Add Lucide icons

--- src/components/base/icon.tsx
+++ src/components/base/icon.tsx
@@ -2,22 +2,38 @@ import ChessComIcon from "@/images/data/chess-com.svg?react";
 import EmailIcon from "@/images/data/email.svg?react";
 import GitHubIcon from "@/images/data/github.svg?react";
 import LinkedInIcon from "@/images/data/linkedin.svg?react";
+import { ExternalLink } from "lucide-react";
 
-const ICONS = {
+const SVG_ICONS = {
   email: EmailIcon,
   github: GitHubIcon,
   linkedin: LinkedInIcon,
   chessCom: ChessComIcon,
 } as const;
 
+const LUCIDE_ICONS = {
+  externalLink: ExternalLink,
+};
+
+const ICONS = { ...SVG_ICONS, ...LUCIDE_ICONS } as const;
+
 type IconProps = {
-  name: keyof typeof ICONS;
+  name: keyof typeof SVG_ICONS | keyof typeof LUCIDE_ICONS;
   alt: string;
   className?: string;
 };
 
 export function Icon({ name, alt, className }: IconProps) {
+  const isLucideIcon = name in LUCIDE_ICONS;
   const IconComponent = ICONS[name];
+
+  if (isLucideIcon) {
+    return (
+      <IconComponent className={`inline-block align-middle ${className}`}>
+        <title>{alt}</title>
+      </IconComponent>
+    );
+  }
 
   return (
     <IconComponent
