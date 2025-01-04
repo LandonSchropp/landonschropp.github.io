f9e293dc92d4bf0c8e8823015c50682ba1084672
Landon Schropp <schroppl@gmail.com>
2025-01-04 10:46:34 -0800

    Add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,25 @@
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "404",
-  description: "The page you were looking for couldn't be found.",
-};
+import { DynamicSVG } from "@/components/dynamic-svg";
+import * as DATA from "@/data/svg";
+
+// import { Metadata } from "next";
+//
+// export const metadata: Metadata = {
+//   title: "404",
+//   description: "The page you were looking for couldn't be found.",
+// };
 
 export default function NotFoundPage() {
-  return null;
+  return (
+    <DynamicSVG>
+      <DynamicSVG.Aspect key="not-found" minSpacing={0} maxSpacing={0}>
+        <DynamicSVG.Group title="404: Not Found" key="heading" role="heading">
+          <DynamicSVG.Row key="heading" align="top" spacing={0}>
+            <DynamicSVG.Shape key="not-found" {...DATA.notFound} />
+          </DynamicSVG.Row>
+        </DynamicSVG.Group>
+      </DynamicSVG.Aspect>
+    </DynamicSVG>
+  );
 }
