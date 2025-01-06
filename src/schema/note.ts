602aa8c42579d2c11a6b73995aba8cdb014c64f3
Landon Schropp <schroppl@gmail.com>
2025-01-05 21:23:27 -0800

    Add ISBN to book notes

--- src/schema/note.ts
+++ src/schema/note.ts
@@ -35,56 +35,51 @@ const NoteSchemaBase = ContentSchema.extend({
   category: z.preprocess(preprocessCategory, CategorySchema),
   media: MediaSchema,
   url: z.string().url(),
+  isbn: z.undefined(),
+  source: z.undefined(),
+  event: z.undefined(),
 });
 
 const ArticleNoteSchema = NoteSchemaBase.extend({
   media: z.literal(ARTICLE_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 const AppNoteSchema = ArticleNoteSchema.extend({
   media: z.literal(APP_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 const BookNoteSchema = NoteSchemaBase.extend({
   authors: NoteSchemaBase.shape.authors.min(1),
   media: z.literal(BOOK_MEDIA),
-  source: z.undefined(),
-  event: z.undefined(),
+  isbn: z.string(),
 });
 
 const CourseNoteSchema = ArticleNoteSchema.extend({
   media: z.literal(COURSE_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 const LiveTalkNoteSchema = NoteSchemaBase.extend({
   authors: NoteSchemaBase.shape.authors.min(1),
   media: z.literal(LIVE_TALK_MEDIA),
-  source: z.undefined(),
   event: z.string(),
 });
 
 const PodcastNoteSchema = ArticleNoteSchema.extend({
   media: z.literal(PODCAST_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 const RecordedTalkNoteSchema = ArticleNoteSchema.extend({
   media: z.literal(RECORDED_TALK_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 const VideoNoteSchema = ArticleNoteSchema.extend({
   media: z.literal(VIDEO_MEDIA),
   source: z.string(),
-  event: z.undefined(),
 });
 
 export const NoteSchema = z.discriminatedUnion("media", [
