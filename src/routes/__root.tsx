ca97996e4213f7735db6dc53bec2de4d3cb29a3d
Landon Schropp <schroppl@gmail.com>
2025-11-16 10:08:36 -0800

    Update tsconfig.json for TanStack Start

---
+++ src/routes/__root.tsx
@@ -0,0 +1,72 @@
+import flannel from "../images/flannel.png";
+import "../styles/index.css";
+import appCss from "../styles/index.css?url";
+import { NAME } from "@/constants";
+import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: NAME,
+      },
+      {
+        name: "description",
+        content: `${NAME} is a developer, designer and entrepreneur based in Portland, OR.`,
+      },
+    ],
+    links: [
+      {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+      },
+      {
+        href:
+          "https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;" +
+          "1,400;1,700&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&family=Source+" +
+          "Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+        rel: "stylesheet",
+      },
+      {
+        rel: "shortcut icon",
+        type: "image/png",
+        href: flannel,
+      },
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
+  component: RootLayout,
+});
+
+function RootLayout() {
+  return (
+    <html lang="en-us" className="h-full text-[19px] md:text-[21px] lg:text-[22px]">
+      <head>
+        <HeadContent />
+      </head>
+      <body
+        className={
+          "bg-theme-background flex h-full flex-col font-serif font-normal " +
+          "text-theme-text *:flex-[0_0_auto]"
+        }
+      >
+        <Scripts />
+      </body>
+    </html>
+  );
+}
